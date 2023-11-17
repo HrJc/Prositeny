1014bd873514336b166f1bae3b8bfedef46bcb4e
m2 manager <lahinirikojudicael@gmail.com>
2023-11-17 10:27:15 +0300

    test112

--- application/config/config.php
+++ application/config/config.php
@@ -24,6 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'http://'.$_SERVER['SERVER_ADDR'].'/Prositeny';
+$config['base_url'] = 'http://102.16.214.93/Prositeny';
 // $config['base_url'] = 'http://192.168.88.93/Prositeny';
 
 /*
